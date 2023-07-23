47169cff8eaaaa18eec03815c1b8d9a154f79902
sslime336 <sslime@yeah.net>
2023-07-23 14:28:59 +0800

    Fix lspsaga comment and rename

--- lua/plugins/config/lspsaga.lua
+++ lua/plugins/config/lspsaga.lua
@@ -4,7 +4,7 @@ local opt = {
     silent = true, noremap = true
 }
 
--- 跳转到声明
+-- 在悬浮窗查看定义
 vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opt)
 -- 跳转到定义
 vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
@@ -13,7 +13,7 @@ vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opt)
 -- 跳转到实现
 vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
 -- 重命名
-vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", opt)
+vim.keymap.set("n", "<space>r", "<cmd>Lspsaga rename<CR>", opt)
 -- 显示错误信息
 vim.keymap.set("n", "<space>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opt)
 -- 跳转到上一个错误
