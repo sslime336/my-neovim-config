c7c0f6f1334354442737a65eabc2ed625fc8d603
sslime336 <sslime@yeah.net>
2023-08-02 18:20:27 +0800

    lsp rust-analyzer checkOnSave.allTargets = false

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -26,7 +26,16 @@ function M.config()
 
     -- Setup LSP
     require("lspconfig").lua_ls.setup { on_attach = on_attach }
-    require("lspconfig").rust_analyzer.setup { on_attach = on_attach }
+    require("lspconfig").rust_analyzer.setup {
+        on_attach = on_attach,
+        settings = {
+            ['rust-analyzer'] = {
+                checkOnSave = {
+                    allTargets = false
+                },
+            },
+        },
+    }
     require("lspconfig").gopls.setup {
         on_attach = on_attach,
         settings = {
