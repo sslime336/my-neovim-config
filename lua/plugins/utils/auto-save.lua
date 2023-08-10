d080e6a154618096c8ad3712d9c1075923e2ae93
sslime336 <sslime@yeah.net>
2023-08-10 12:52:52 +0800

    Tidy auto-save.nvim

--- lua/plugins/utils/auto-save.lua
+++ lua/plugins/utils/auto-save.lua
@@ -1,12 +1,15 @@
 -- 自动保存
 
-return {
+local M = {
     "Pocco81/auto-save.nvim",
     event = { "BufReadPost" },
-    config = function()
-        require("auto-save").setup {
-            -- 一次性保存所有 buffer
-            write_all_buffers = true,
-        }
-    end,
 }
+
+M.config = function()
+    require("auto-save").setup {
+        -- 一次性保存所有 buffer
+        write_all_buffers = true,
+    }
+end
+
+return M
