606c1092e0a8078f18128d44bc231ee485ffdb1d
sslime336 <sslime@yeah.net>
2023-08-12 15:32:06 +0800

    Tidy Go support plugin

--- lua/plugins/languages/go.lua
+++ lua/plugins/languages/go.lua
@@ -1,7 +1,7 @@
 -- A feature-rich Go development plugin, leveraging gopls, treesitter AST,
 -- Dap, and various Go tools to enhance the development experience.
 
-return {
+local M = {
     "ray-x/go.nvim",
     ft = { "go", 'gomod', 'gowork' },
     dependencies = {
@@ -9,14 +9,17 @@ return {
         "neovim/nvim-lspconfig",
         "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-        local capabilities = require('cmp_nvim_lsp')
-            .default_capabilities(vim.lsp.protocol.make_client_capabilities())
-        require('go').setup {
-            lsp_cfg = {
-                capabilities = capabilities,
-            },
-        }
-    end,
     build = ':lua require("go.install").update_all_sync()',
 }
+
+M.config = function()
+    local capabilities = require('cmp_nvim_lsp')
+        .default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    require('go').setup {
+        lsp_cfg = {
+            capabilities = capabilities,
+        },
+    }
+end
+
+return M
